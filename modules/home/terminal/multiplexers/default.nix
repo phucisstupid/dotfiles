e52a31780c529e51690c2288f87233fe9527e164
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-06-22 14:48:44 +0700

    tmux format

--- modules/home/terminal/multiplexers/default.nix
+++ modules/home/terminal/multiplexers/default.nix
@@ -4,9 +4,11 @@
   lib,
   flake,
   ...
-}: let
+}:
+let
   inherit (flake.config.me) namespace;
-in {
+in
+{
   options.${namespace}.terminal.multiplexers = {
     tmux.enable = lib.mkEnableOption "tmux";
     zellij.enable = lib.mkEnableOption "zellij";
@@ -14,14 +16,14 @@ in {
   config = lib.mkMerge [
     (lib.mkIf config.${namespace}.terminal.multiplexers.tmux.enable {
       catppuccin.tmux.extraConfig = ''
-                        set -g @catppuccin_window_status_style "rounded"
+        set -g @catppuccin_window_status_style "rounded"
+        set -g @catppuccin_window_current_text " #W"
         set -g @catppuccin_window_text " #W "
-                set -g @catppuccin_window_current_text " #W"
-                set -g @catppuccin_window_flags "icon"
-                        set -g status-left "#{E:@catppuccin_status_session}"
-                        set -g status-right "#{E:@catppuccin_status_directory}"
-                        set -agF status-right "#{E:@catppuccin_status_cpu}"
-                        set -agF status-right "#{E:@catppuccin_status_weather}"
+        set -g @catppuccin_window_flags "icon"
+        set -g status-left "#{E:@catppuccin_status_session}"
+        set -g status-right "#{E:@catppuccin_status_directory}"
+        set -agF status-right "#{E:@catppuccin_status_cpu}"
+        set -agF status-right "#{E:@catppuccin_status_weather}"
       '';
       programs.tmux = {
         enable = true;
@@ -32,9 +34,9 @@ in {
         disableConfirmationPrompt = true;
         tmuxinator.enable = true;
         plugins = with pkgs.tmuxPlugins; [
-          yank
           cpu
           weather
+          yank
           vim-tmux-navigator
         ];
         extraConfig = ''
