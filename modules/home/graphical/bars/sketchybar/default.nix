dd170dd6f8d3ded61a8fb1bca7ebb729123d2d5c
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-06-26 14:09:47 +0700

    comeback to old method

--- modules/home/graphical/bars/sketchybar/default.nix
+++ modules/home/graphical/bars/sketchybar/default.nix
@@ -4,9 +4,11 @@
   lib,
   flake,
   ...
-}: let
+}:
+let
   inherit (flake.config.me) namespace;
-in {
+in
+{
   options.${namespace}.graphical.bars.sketchybar.enable = lib.mkEnableOption "sketchybar";
   config = lib.mkIf config.${namespace}.graphical.bars.sketchybar.enable {
     home.packages = with pkgs; [
@@ -28,7 +30,10 @@ in {
       ];
     };
     xdg.configFile = {
-      "sketchybar/helpers/icon_map.lua".source = "${pkgs.sketchybar-app-font}/lib/sketchybar-app-font/icon_map.lua";
+      "sketchybar/helpers/app_icons.lua".source =
+        "${pkgs.sketchybar-app-font}/lib/sketchybar-app-font/icon_map.lua";
+      "sketchybar/helpers/icon_map.lua".source =
+        "${pkgs.sketchybar-app-font}/lib/sketchybar-app-font/icon_map.lua";
       "sketchybar/sketchybarrc" = {
         executable = true;
         text = ''
