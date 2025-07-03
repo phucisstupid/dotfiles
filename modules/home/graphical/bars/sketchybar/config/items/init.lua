30ffd1a034457bc0f82dd9c0556af387cf611bc2
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-07-03 13:15:30 +0700

    update sketchybar config

--- modules/home/graphical/bars/sketchybar/config/items/init.lua
+++ modules/home/graphical/bars/sketchybar/config/items/init.lua
@@ -3,6 +3,9 @@ require("items.apple")
 require("items.spaces")
 require("items.menus")
 
+--center
+-- require("items.notifications")
+
 --right (reverse order)
 require("items.calendar")
 require("items.widgets")
