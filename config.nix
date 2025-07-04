34d776eb7df1af822f1adc4a78c76cd2ec0a3139
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-07-04 23:08:29 +0700

    minor

--- config.nix
+++ config.nix
@@ -2,7 +2,7 @@
 {
   me = {
     username = "wow";
-    name = "phucleeuwu";
+    name = "phucisstupid";
     email = "125681538+phucleeuwu@users.noreply.github.com";
     sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQRxPoqlThDrkR58pKnJgmeWPY9/wleReRbZ2MOZRyd";
     namespace = "phuc";
