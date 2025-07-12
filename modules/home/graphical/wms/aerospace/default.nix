e6903bb5698f27f578ac29b90899b83a6dff05c6
phucisstupid <125681538+phucleeuwu@users.noreply.github.com>
2025-07-12 15:07:43 +0700

    add apple tv

--- modules/home/graphical/wms/aerospace/default.nix
+++ modules/home/graphical/wms/aerospace/default.nix
@@ -4,9 +4,11 @@
   lib,
   flake,
   ...
-}: let
+}:
+let
   inherit (flake.config.me) namespace;
-in {
+in
+{
   options.${namespace}.graphical.wms.aerospace.enable = lib.mkEnableOption "aerospace";
   config = lib.mkIf config.${namespace}.graphical.wms.aerospace.enable {
     programs.aerospace = {
@@ -32,7 +34,7 @@ in {
           ])
         ];
         on-focus-changed = lib.mkMerge [
-          ["move-mouse window-lazy-center"]
+          [ "move-mouse window-lazy-center" ]
           (lib.optionals config.${namespace}.graphical.bars.simple-bar.enable [
             "exec-and-forget osascript -e 'tell application id \"tracesOf.Uebersicht\" to refresh widget id \"simple-bar-index-jsx\"'"
           ])
@@ -47,11 +49,12 @@ in {
           };
           outer = {
             top = lib.mkDefault (
-              if config.${namespace}.graphical.bars.sketchybar.enable
-              then 45
-              else if config.${namespace}.graphical.bars.simple-bar.enable
-              then 30
-              else 5
+              if config.${namespace}.graphical.bars.sketchybar.enable then
+                45
+              else if config.${namespace}.graphical.bars.simple-bar.enable then
+                30
+              else
+                5
             );
             bottom = 5;
             left = 5;
@@ -135,7 +138,7 @@ in {
             run = "move-node-to-workspace 1";
           }
           {
-            "if".app-name-regex-substring = "spotify|stremio|hayase";
+            "if".app-name-regex-substring = "TV|spotify|stremio|hayase";
             run = "move-node-to-workspace 2";
           }
           {
