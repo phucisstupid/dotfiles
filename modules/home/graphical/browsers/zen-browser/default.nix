eb2d629471250058c9ea5d85e4f75ea60b28d6e9
phucisstupid <125681538+phucleeuwu@users.noreply.github.com>
2025-08-10 13:47:18 +0700

    fix zen-browser

--- modules/home/graphical/browsers/zen-browser/default.nix
+++ modules/home/graphical/browsers/zen-browser/default.nix
@@ -4,13 +4,13 @@
   lib,
   flake,
   ...
-}: {
+}:
+{
   options.${flake.config.me.namespace}.graphical.browsers.zen-browser.enable =
     lib.mkEnableOption "zen-browser";
   config = lib.mkIf config.${flake.config.me.namespace}.graphical.browsers.zen-browser.enable {
     programs.zen-browser = {
       enable = true;
-      nativeMessagingHosts = [pkgs.firefoxpwa];
       policies = {
         AutofillAddressEnabled = true;
         AutofillCreditCardEnabled = false;
