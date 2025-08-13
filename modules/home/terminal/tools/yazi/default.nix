e2bff8aebbdf9ae590a7205bb5d21fcfadcf194e
phucisstupid <125681538+phucleeuwu@users.noreply.github.com>
2025-08-14 00:42:45 +0700

    feat(yazi): add more useful plugins

--- modules/home/terminal/tools/yazi/default.nix
+++ modules/home/terminal/tools/yazi/default.nix
@@ -4,32 +4,36 @@
   lib,
   flake,
   ...
-}: let
+}:
+let
   inherit (flake.config.me) namespace;
   inherit (flake) inputs;
-in {
+in
+{
   options.${namespace}.terminal.tools.yazi.enable = lib.mkEnableOption "yazi";
   config = lib.mkIf config.${namespace}.terminal.tools.yazi.enable {
     programs.yazi = {
       enable = true;
       shellWrapperName = "y"; # TODO: clean when default is fixed
-      plugins =
-        {
-          inherit
-            (pkgs.yaziPlugins)
-            git
-            smart-filter
-            full-border
-            toggle-pane
-            smart-enter
-            yatline
-            yatline-catppuccin
-            relative-motions
-            ;
-        }
-        // lib.optionalAttrs config.${namespace}.terminal.tools.lazygit.enable {
-          inherit (pkgs.yaziPlugins) lazygit;
-        };
+      plugins = {
+        inherit (pkgs.yaziPlugins)
+          git
+          vcs-files
+          mount
+          chmod
+          jump-to-char
+          full-border
+          toggle-pane
+          smart-enter
+          smart-paste
+          yatline
+          yatline-catppuccin
+          relative-motions
+          ;
+      }
+      // lib.optionalAttrs config.${namespace}.terminal.tools.lazygit.enable {
+        inherit (pkgs.yaziPlugins) lazygit;
+      };
       initLua = builtins.readFile "${inputs.dotfiles-stow}/yazi/init.lua";
       settings = {
         mgr.show_hidden = true;
@@ -50,34 +54,55 @@ in {
           }
         ];
       };
-      keymap.mgr.prepend_keymap =
-        [
-          {
-            on = "T";
-            run = "plugin toggle-pane max-preview";
-          }
-          {
-            on = "F";
-            run = "plugin smart-filter";
-          }
-          {
-            on = "l";
-            run = "plugin smart-enter";
-          }
-        ]
-        ++ builtins.map (n: {
-          on = "${toString n}";
-          run = "plugin relative-motions ${toString n}";
-        }) (lib.range 1 9)
-        ++ lib.optionals config.${namespace}.terminal.tools.lazygit.enable [
-          {
-            on = [
-              "g"
-              "i"
-            ];
-            run = "plugin lazygit";
-          }
-        ];
+      keymap.mgr.prepend_keymap = [
+        {
+          on = [
+            "g"
+            "c"
+          ];
+          run = "plugin vcs-files";
+        }
+        {
+          on = "p";
+          run = "plugin smart-paste";
+        }
+        {
+          on = "T";
+          run = "plugin toggle-pane max-preview";
+        }
+        {
+          on = "M";
+          run = "plugin mount";
+        }
+        {
+          on = "l";
+          run = "plugin smart-enter";
+        }
+        {
+          on = "f";
+          run = "plugin jump-to-char";
+        }
+        {
+          on = [
+            "c"
+            "m"
+          ];
+          run = "plugin chmod";
+        }
+      ]
+      ++ builtins.map (n: {
+        on = "${toString n}";
+        run = "plugin relative-motions ${toString n}";
+      }) (lib.range 1 9)
+      ++ lib.optionals config.${namespace}.terminal.tools.lazygit.enable [
+        {
+          on = [
+            "g"
+            "i"
+          ];
+          run = "plugin lazygit";
+        }
+      ];
     };
   };
 }
