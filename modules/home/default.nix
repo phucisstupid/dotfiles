58c447bbe90754508a871446fd32f6fdc901010e
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-08-30 11:36:49 +0700

    feat: enable jj by default

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,5 +1,6 @@
-{flake, ...}: {
-  imports = [./import.nix];
+{ flake, ... }:
+{
+  imports = [ ./import.nix ];
   xdg.enable = true;
   home = {
     stateVersion = "25.05";
@@ -62,7 +63,7 @@
         tealdeer.enable = true;
         nh.enable = true;
         gh-dash.enable = true;
-        jujutsu.enable = false;
+        jujutsu.enable = true;
         btop.enable = true;
         bottom.enable = false;
         navi.enable = false;
