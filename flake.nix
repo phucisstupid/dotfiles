ddcacdd197d2850ceb6e41256693fd448d8db40f
phucisstupid phucisstupid@users.noreply.github.com <phucisstupid phucisstupid@users.noreply.github.com>
2025-09-02 08:29:34 +0000

    Chore: Automated dead code cleanup

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  outputs = inputs @ {self, ...}:
+  outputs = inputs @ { ...}:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
       imports = [(inputs.import-tree ./modules/flake)];
