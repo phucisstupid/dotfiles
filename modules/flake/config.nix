5750725f8d446588ecf835ae4bf554182408e410
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-09-15 00:07:21 +0700

    better

--- modules/flake/config.nix
+++ modules/flake/config.nix
@@ -1,28 +1,29 @@
 # Top-level configuration for everything in this repo.
 #
 # Values are set in 'config.nix' in repo root.
-{lib, ...}: let
-  userSubmodule = lib.types.submodule {
-    options = {
+{ config, lib, ... }:
+{
+  imports = [
+    ../../config.nix
+  ];
+  options = {
+    me = {
       username = lib.mkOption {
         type = lib.types.str;
+        description = "Your username as shown by `id -un`";
       };
       name = lib.mkOption {
         type = lib.types.str;
+        description = "Your full name for use in Git config";
       };
       email = lib.mkOption {
         type = lib.types.str;
+        description = "Your email for use in Git config";
       };
       namespace = lib.mkOption {
         type = lib.types.str;
       };
     };
-  };
-in {
-  imports = [../../config.nix];
-  options = {
-    me = lib.mkOption {
-      type = userSubmodule;
-    };
+    terminal.enable = lib.mkEnableOption "Enable terminal configuration";
   };
 }
