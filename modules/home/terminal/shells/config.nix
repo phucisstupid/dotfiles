51b5ea06a3714b83eeb596a2800f9dbfc628e1a7
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-09-17 23:19:12 +0700

    feat(nushell): remove aliases

--- modules/home/terminal/shells/config.nix
+++ modules/home/terminal/shells/config.nix
@@ -4,9 +4,11 @@
   flake,
   lib,
   ...
-}: let
+}:
+let
   inherit (flake.config.me) namespace;
-in {
+in
+{
   options.${namespace}.terminal.shells = {
     zsh.enable = lib.mkEnableOption "zsh";
     fish.enable = lib.mkEnableOption "fish";
@@ -27,8 +29,8 @@ in {
         autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
         plugins = with pkgs; [
-          {inherit (zsh-fzf-tab) name src;}
-          {inherit (zsh-vi-mode) name src;}
+          { inherit (zsh-fzf-tab) name src; }
+          { inherit (zsh-vi-mode) name src; }
         ];
       };
       fish = {
@@ -39,7 +41,7 @@ in {
           fish_vi_key_bindings
         '';
         plugins = with pkgs; [
-          {inherit (fishPlugins.fzf-fish) name src;}
+          { inherit (fishPlugins.fzf-fish) name src; }
         ];
       };
       nushell = {
@@ -47,12 +49,6 @@ in {
         settings = {
           show_banner = false;
           edit_mode = "vi";
-        };
-        shellAliases = {
-          ls = "ls";
-          ll = "ls -l";
-          la = "ls -a";
-          lla = "ls -la";
         };
       };
     };
