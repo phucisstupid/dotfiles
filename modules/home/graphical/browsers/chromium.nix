01ec9e58f09b9540987b95136e352f1df4209d01
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-09-18 23:16:46 +0700

    refactor

--- modules/home/graphical/browsers/chromium.nix
+++ modules/home/graphical/browsers/chromium.nix
@@ -3,40 +3,44 @@
   lib,
   flake,
   ...
-}: let
+}:
+let
   inherit (flake.config.me) namespace;
-in {
+in
+{
   options.${namespace}.graphical.browsers = {
     chromium.enable = lib.mkEnableOption "chromium";
     brave.enable = lib.mkEnableOption "brave";
   };
-  config.programs = {
-    chromium = {
-      inherit (config.${namespace}.graphical.browsers.chromium) enable;
-      extensions = [
-        "ddkjiahejlhfcafbddmgiahcphecmpfh" # uBlock Origin Lite
-        "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
-        "clngdbkpkpeebahjckkjfobafhncgmne" # Stylus
-        "pejdijmoenmkgeppbflobdenhhabjlaj" # iCloud Passwords
-        "lnjaiaapbakfhlbjenjkhffcdpoompki" # Catppuccin Web File Icons
-        "hlepfoohegkhhmjieoechaddaejaokhf" # Refined GitHub
-        "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock
-        "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
-        "lodbfhdipoipcjmlebjbgmmgekckhpfb" # Harper
-      ];
-    };
-    brave = {
-      inherit (config.${namespace}.graphical.browsers.brave) enable;
-      extensions = [
-        "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
-        "clngdbkpkpeebahjckkjfobafhncgmne" # Stylus
-        "pejdijmoenmkgeppbflobdenhhabjlaj" # iCloud Passwords
-        "lnjaiaapbakfhlbjenjkhffcdpoompki" # Catppuccin Web File Icons
-        "hlepfoohegkhhmjieoechaddaejaokhf" # Refined GitHub
-        "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock
-        "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
-        "lodbfhdipoipcjmlebjbgmmgekckhpfb" # Harper
-      ];
+  config = with config.${namespace}.graphical.browsers; {
+    programs = {
+      chromium = {
+        inherit (chromium) enable;
+        extensions = [
+          "ddkjiahejlhfcafbddmgiahcphecmpfh" # uBlock Origin Lite
+          "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+          "clngdbkpkpeebahjckkjfobafhncgmne" # Stylus
+          "pejdijmoenmkgeppbflobdenhhabjlaj" # iCloud Passwords
+          "lnjaiaapbakfhlbjenjkhffcdpoompki" # Catppuccin Web File Icons
+          "hlepfoohegkhhmjieoechaddaejaokhf" # Refined GitHub
+          "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock
+          "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
+          "lodbfhdipoipcjmlebjbgmmgekckhpfb" # Harper
+        ];
+      };
+      brave = {
+        inherit (brave) enable;
+        extensions = [
+          "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+          "clngdbkpkpeebahjckkjfobafhncgmne" # Stylus
+          "pejdijmoenmkgeppbflobdenhhabjlaj" # iCloud Passwords
+          "lnjaiaapbakfhlbjenjkhffcdpoompki" # Catppuccin Web File Icons
+          "hlepfoohegkhhmjieoechaddaejaokhf" # Refined GitHub
+          "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock
+          "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
+          "lodbfhdipoipcjmlebjbgmmgekckhpfb" # Harper
+        ];
+      };
     };
   };
 }
