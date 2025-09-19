146d82f327fe3fd7b9414e5576c30d9a332c56ba
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-09-19 15:14:07 +0700

    feat(jankyborders): use hidpi

--- modules/home/services/jankyborders.nix
+++ modules/home/services/jankyborders.nix
@@ -3,15 +3,17 @@
   lib,
   flake,
   ...
-}: let
+}:
+let
   inherit (flake.config.me) namespace;
-in {
+in
+{
   options.${namespace}.services.jankyborders.enable = lib.mkEnableOption "jankyborders";
   config.services.jankyborders = {
     inherit (config.${namespace}.services.jankyborders) enable;
     settings = {
-      order = "below";
       active_color = "0xffcba6f7";
+      hidpi = "on";
     };
   };
 }
