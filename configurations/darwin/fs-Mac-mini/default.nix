33714ebd53de313bbc9298f850aabb4ea79dfd6d
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-10-15 18:16:29 +0700

    add new host

---
+++ configurations/darwin/fs-Mac-mini/default.nix
@@ -0,0 +1,21 @@
+{ flake, ... }:
+let
+  inherit (flake) inputs;
+in
+{
+  imports = [
+    (inputs.import-tree ../../../modules/darwin)
+  ];
+  nix.enable = false; # for Determinate Nix
+  networking.hostName = "fs-Mac-mini";
+  nixpkgs = {
+    hostPlatform = "aarch64-darwin";
+    config.allowUnfree = true;
+  };
+  system.stateVersion = 6;
+  security.pam.services.sudo_local = {
+    touchIdAuth = true;
+    reattach = true;
+  };
+  home-manager.backupFileExtension = "backup";
+}
