c79bc9e873933cd9c16796de4af9d66c73ce1da2
phucisstupid <phucleeuwu@gmail.com>
2026-08-01 14:04:48 +0700

    update obs

--- modules/aspects/apps/obs-studio.nix
+++ modules/aspects/apps/obs-studio.nix
@@ -1,11 +1,8 @@
-{pkgs, ...}: {
+_: {
   den.aspects.app.obs-studio = {
     homeManager = {
       programs.obs-studio = {
         enable = true;
-        plugins = with pkgs.obs-studio-plugins; [
-          droidcam-obs
-        ];
       };
     };
   };
