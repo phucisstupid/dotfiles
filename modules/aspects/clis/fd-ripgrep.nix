1fd128ec1ca088744809e13c47d96cbe31e716cc
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2026-09-13 03:05:25 +0700

    update

--- modules/aspects/clis/fd-ripgrep.nix
+++ modules/aspects/clis/fd-ripgrep.nix
@@ -7,7 +7,6 @@ _: {
           hidden = true;
           ignores = [
             ".git/"
-            "*.bak"
           ];
         };
       };
@@ -25,9 +24,11 @@ _: {
             "--smart-case"
           ];
         };
+
         programs.bat = {
           extraPackages = with pkgs.bat-extras; [batgrep];
         };
+
         home.shellAliases = {
           rg = "batgrep";
         };
